d84874896b2706c974e1b56c3dcfaa53d2f56a1b
sabin-kratos <sabinmasangri01@gmail.com>
2025-12-23 15:24:36 +0000

    IF & ELSE

--- bash-scripting/p1.sh
+++ bash-scripting/p1.sh
@@ -4,18 +4,23 @@ echo "Welcome $username. How can i help you in your calculation."
 read -p "Enter First number= " a
 read -p "Enter second number= " b
 echo "Select what you want to do: + - / * "
-if [[ + ]]; then
-sum = $((a+b))
+read -p "Enter your operation: " c
+if [[ "$c" == "+" ]]; then
+sum=$((a + b))
 echo "sum=$sum"
-elif [[ - ]]; then
-diff= $((a-b))
+elif [["$c" == "-" ]]; then
+diff=$((a - b))
 echo "diff=$diff"
-elif [[ * ]]; then 
-product= $((a*b))
+elif [[ "$c" == "*" ]]; then 
+product=$((a * b))
 echo "product=$product"
-elif [[ / ]]; then 
-div= $((a/b))
+elif [[ "$c" == "/" ]]; then 
+if [[ "$b" -eq 0 ]]; then
+echo "Not divisible by 0"
+else
+div=$((a / b))
 echo "div=$div"
+fi
 else
 echo "Error"
 fi
