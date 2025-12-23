d12869b3a4550cd2c0897e2a611360edbe17ab33
sabin-kratos <sabinmasangri01@gmail.com>
2025-12-23 10:10:59 +0000

    initial

--- bash-scripting/p1.sh
+++ bash-scripting/p1.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 read -p "Write your name= " username
-echo "Welcome $username "
+echo "Welcome $username. How can i help you in your calculation."
+read -p "Enter First number= " a
+read -p "Enter second number= " b
+echo "Select what you want to do: + - / * "
+if [[ + ]]; then
+sum = $((a+b))
+echo "sum=$sum"
+elif [[ - ]]; then
+diff= $((a-b))
+echo "diff=$diff"
+elif [[ * ]]; then 
+product= $((a*b))
+echo "product=$product"
+elif [[ / ]]; then 
+div= $((a/b))
+echo "div=$div"
+else
+echo "Error"
+fi
 
