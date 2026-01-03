8ba7676b0c53b947cf4cef5d5cbabe50dd2f7461
sabin-kratos <sabinmasangri01@gmail.com>
2026-01-03 16:49:29 +0000

    while_loop

---
+++ bash-scripting/while.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Write a Bash script that asks the user for a number and then counts down to zero using a while loop.
+
+read -p "Enter a number: " n
+while [ $n -ge 0 ]
+do 
+echo  $n
+n=$(( $n-1 ))
+done
+
+
+#Create a script that keeps asking the user to enter numbers until they type 0. Use a while loop to calculate the sum of all entered numbers.
+
+sum=0
+read -p "Enter number (0 to stop): " num
+while [ $num -ne 0 ]
+do 
+sum=$(( sum+num ))
+read -p "Enter number (0 to stop): " num
+done
+echo "sum=$sum"
+
+
+#Write a script that keeps asking the user to enter a password until they type "secret". Once they enter the correct password, print "Access Granted".
+
+read -p "Enter the password: " pass
+while [ $pass != "secret" ]
+do 
+echo "Wrong password"
+read -p "Enter the password: " pass
+done
+echo "Access Granted"
+
+#from ai
+
+pass="secret"
+input=""
+while [ "$input" != "$pass" ]
+do 
+echo  "Enter password: "
+read input
+done
+echo "Access granted"
+
+#Use a while loop to check the disk usage of /home every 5 seconds. Stop the loop if usage goes above 80%.
+
+while [ 
