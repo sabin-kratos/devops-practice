0c9afe1f39fba6663d2144ea8b127711e27bfa52
sabin-kratos <sabinmasangri01@gmail.com>
2026-02-13 16:41:45 +0000

    New one

--- bash-scripting/file.sh
+++ bash-scripting/file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Enter the number:"
-echo -e "1) file /n2) dictionary"
+echo -e "1) file \n2) dictionary \n3) diskusage"
 read a
 case $a in
 1) 
@@ -11,6 +11,9 @@ touch $file
 2)
 read -p "Enter dictionary name:" dic
 mkdir $dic
+;;
+3)
+df -h 
 ;;
 *)
 echo "Invalid number:"
